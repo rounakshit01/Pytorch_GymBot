5ef17e8321d25f64c35533c3ae4f015dbda430fa
Rounak <93839114+rounakshit01@users.noreply.github.com>
2024-11-19 03:04:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,4 +64,4 @@ def chat():
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 4000))  # Allow dynamic port binding for deployment
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
